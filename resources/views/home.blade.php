8a65edcf3b4bb572885d2441d6832c6879ad3b77
manuelgaleone <galeonemanuel14@gmail.com>
2022-12-16 16:11:31 +0100

    Add header and footer, fixing imgs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,57 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
     <title>Laravel Comics</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    <div class="header_main">
-        <div class="container">
-            <div class="row align_items justify_end header_content">
-                <div class="logo_header col-4">
-                    <img class="logo" src="../images/dc-logo.png" alt="Comics Logo">
-                </div>
-                <div class="menu_header col-8">
-                    <div class="menu_elements">
-                        <ul class="d_flex align_items">
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">CHARACTERS</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item active" href="#">COMICS</a>
-                                <div class="rectangle_active">
+    @include('partials.header')
+    <div class="main_content">
 
-                                </div>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">MOVIES</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">TV</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">GAMES</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">COLLECTIBLES</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">VIDEOS</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">FANS</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">NEWS</a>
-                            </li>
-                            <li class="menu_element">
-                                <a class="menu_item" href="#">SHOP</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
+    @include('partials.footer')
 </body>
 
 </html>
