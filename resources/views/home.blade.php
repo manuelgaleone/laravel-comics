78fff922a83ba87638152db677caea0185e69e7b
manuelgaleone <galeonemanuel14@gmail.com>
2022-12-16 15:50:58 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+    <title>Laravel Comics</title>
+</head>
+
+<body>
+    <div class="header_main">
+        <div class="container">
+            <div class="row align_items justify_end header_content">
+                <div class="logo_header col-4">
+                    <img class="logo" src="../images/dc-logo.png" alt="Comics Logo">
+                </div>
+                <div class="menu_header col-8">
+                    <div class="menu_elements">
+                        <ul class="d_flex align_items">
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">CHARACTERS</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item active" href="#">COMICS</a>
+                                <div class="rectangle_active">
+
+                                </div>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">MOVIES</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">TV</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">GAMES</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">COLLECTIBLES</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">VIDEOS</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">FANS</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">NEWS</a>
+                            </li>
+                            <li class="menu_element">
+                                <a class="menu_item" href="#">SHOP</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
